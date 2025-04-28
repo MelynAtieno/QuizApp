a2c039526ea26481c5a6e588da00ba54b3d792ee
Melyn Were <WereMA@cardiff.ac.uk>
2025-04-28 10:45:15 +0100

    fetch api data

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple"
+const api_url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple"
 constProgress = document.getElementById("progress")
 constQuestions = document.getElementById("questions")
 constAnswers = document.getElementById("answers")
 
-fetch(url);
+
+fetch(api_url)
+    .then(response => {
+        if(!response.ok){
+            throw new Error("No response")
+        }
+        return response.json();
+    })
+
+
