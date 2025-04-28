4c69080c61dff1682969222c3ce75495fe79913a
Melyn Were <WereMA@cardiff.ac.uk>
2025-04-28 10:37:34 +0100

    add reference to questions, answers and progress container.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple"
+constProgress = document.getElementById("progress")
+constQuestions = document.getElementById("questions")
+constAnswers = document.getElementById("answers")
 
 fetch(url);
