28708a5511a3af18b3e953dccc0ddb6e0abe98c7
Melyn Were <WereMA@cardiff.ac.uk>
2025-05-07 21:23:41 +0100

    fetch and display question

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const api_url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple"
+const quizContainer = document.getElementById("quiz-container")
 const progress = document.getElementById("progress")
-const questions = document.getElementById("question")
-const answers = document.getElementById("answer")
-const next = document.getElementById("next-question")
-const previous = document.getElementById("previous-question")
+const questionText = document.getElementById("questionText")
+const answersText = document.getElementById("answer")
+const nextBtn = document.getElementById("next-question")
+const previousBtn = document.getElementById("previous-question")
 let currentQuestionIndex = 0;
+let score = 0;
+let  quizzes = [];
 
 fetch(api_url)
-    .then(response => {
-        if(!response.ok){
-            throw new Error("No response")
-        }
-        return response.json();
-    })
-
-function handleProgress(){
-    questions.forEach((question) => {
-        progress.innerHTML += `<span></span>`
+    .then(res => res.json())
+    .then(data => {
+        quizzes = data.results
+        displayQuestion(currentQuestionIndex);
     });
 
-    questions.innerHTML = `<p>${question[index]}</p>`
+function displayQuestion(index){
+    const q = quizzes[index];
+    questionText.innerHTML = q.question;
+
 }
-handleProgress(currentQuestionIndex);
+
+
 
 
 
